3b9e9acac4415193a96c82130a5b4a63f88a54f8
ShiivRS331 <220701331@rajalakshmi.edu.in>
2024-09-18 08:47:22 +0530

    Update displaycust.php

--- Record/Ex 8/displaycust.php
+++ Record/Ex 8/displaycust.php
@@ -1,10 +1,13 @@
 <?php
 $cid = $_POST["cid"];
 echo "<h1>Customer Details</h1><hr>";
-echo "Customer ID: ".$cid;
-$conn = new mysqli("localhost", "root", "", "bank");
-$sql = "SELECT from customer where cust_id='$cid'";
+echo "<p><b>Customer ID: </b>".$cid."</p>";
+$conn = new mysqli("localhost", "root","", "bank");
+$sql = "SELECT * from customer where cust_id=$cid";
 $result = $conn->query($sql);
-$row = $result->fetch_assoc();
-echo "Customer Name: ".$row["cust_name"];
+while($row = $result->fetch_assoc()){
+	echo "<p><b>Customer Name: </b>".$row["cust_name"]."</p>";
+}
+
+$conn->close();
 ?>
